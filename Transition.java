e47992c779d9075a5759d4c1fb47906e502b75c0
Jinsik Kim <39106724+jkim176@users.noreply.github.com>
2018-10-16 06:14:25 -0400

    Update Transition.java
    -changed from Map<Map<State, Character>, State> ... to ... Map<State, Map<Character, State>>
    -edit method addTransition
    -edit method getTarget

--- Transition.java
+++ Transition.java
@@ -4,43 +4,49 @@ import java.util.Map;
 import java.util.HashMap;
 
 class Transition {
-   private Map<Map<State, Character>, State> transition = new HashMap<>();
+   private Map<State, Map<Character, State>> transition = new HashMap<>();
    
    Transition() {
    
    }
    
-   Map<Map<State, Character>, State> getTransitionFunction() {
+   Map<State, Map<Character, State>> getTransitionFunction() {
       return transition;
    }
    
    void addTransition(State source, char input, State target) {
-      Map<State, Character> temp = new HashMap<>();
-      temp.put(source, input);
-      transition.put(temp, target);
+      if(!transition.containsKey(source)) {     // if key source is not in transition, add new Map
+         Map<Character, State> value = new HashMap<>();
+         transition.put(source, value);
+         
+         value.put(input, target);     // put into nested Map
+      }
+      else {
+         Map<Character, State> value = transition.get(source);    // else retrieve nested map
+         
+         value.put(input, target);
+      }
    }
    
    State getTarget(State source, char input) {
-      for(Map.Entry<Map<State, Character>, State> entry: transition.entrySet()) {
-         Map<State, Character> key = entry.getKey();
-         State target = entry.getValue();
-         
-         if(key.containsKey(source) && key.containsValue(input)) {
-            return target;
-         }
+      State target = transition.get(source).get(input);
+      if(target == null) {
+         throw new NullPointerException("NPException: Transition not found for source: " + source.getName() + " input: " + input);
       }
-      throw new IllegalArgumentException("IAException: No transition found for source: " + source + " char: " + input + "\n");    // if no target is returned, transition does not exist
+      
+      else
+         return target;
    }
    
    void printTransition() {
       System.out.print("Transition Function: \n");
-      for(Map.Entry<Map<State, Character>, State> entry: transition.entrySet()) {  // for each entry in transitions map
-         Map<State, Character> key = entry.getKey();     // get key, which is a single-entry nested Map(source state, input char)
-         State target = entry.getValue();       // get value, target State
+      for(Map.Entry<State, Map<Character, State>> entry: transition.entrySet()) {  // for each entry in transitions map
+         State source = entry.getKey();     // get key, source State
+         Map<Character, State> value = entry.getValue();       // get value, single-entry Map(input Character, target State)
          
-         for(Map.Entry<State, Character> e: key.entrySet()) {     // for each entry(1) in nested Map...
-            State source = e.getKey();
-            char input = e.getValue();
+         for(Map.Entry<Character, State> e: value.entrySet()) {     // for each entry(1) in nested Map...
+            char input = e.getKey();
+            State target = e.getValue();
             System.out.print("\tSource State " + source.getName() + " --> input " + input + " --> Target State " + target.getName() + "\n");
          }       
       }
